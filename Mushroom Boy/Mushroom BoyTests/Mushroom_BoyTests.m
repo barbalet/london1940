67b97f4ddadc1228733a6ee1212ad09cd1d59562
Tom Barbalet <tom@nobleape.com>
2014-06-21 20:20:19 -0700

    Added Mac project and a slight re-org
    See title.

---
+++ Mushroom Boy/Mushroom BoyTests/Mushroom_BoyTests.m
@@ -0,0 +1,34 @@
+//
+//  Mushroom_BoyTests.m
+//  Mushroom BoyTests
+//
+//  Created by Tom Barbalet on 6/21/14.
+//  Copyright (c) 2014 Tom Barbalet. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Mushroom_BoyTests : XCTestCase
+
+@end
+
+@implementation Mushroom_BoyTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
---
+++ gui/shared.c
@@ -0,0 +1,166 @@
+/****************************************************************
+
+ shared.h
+
+ =============================================================
+
+ Copyright 1996-2014 Tom Barbalet. All rights reserved.
+
+ Permission is hereby granted, free of charge, to any person
+ obtaining a copy of this software and associated documentation
+ files (the "Software"), to deal in the Software without
+ restriction, including without limitation the rights to use,
+ copy, modify, merge, publish, distribute, sublicense, and/or
+ sell copies of the Software, and to permit persons to whom the
+ Software is furnished to do so, subject to the following
+ conditions:
+
+ The above copyright notice and this permission notice shall be
+ included in all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ OTHER DEALINGS IN THE SOFTWARE.
+
+ This software and Noble Ape are a continuing work of Tom Barbalet,
+ begun on 13 June 1996. No apes or cats were harmed in the writing
+ of this software.
+
+ ****************************************************************/
+
+#include "shared.h"
+#include <stdio.h>
+
+static n_byte  screen[1024 * 768] = {0};
+
+extern n_int draw_error(n_constant_string error_text, n_constant_string location, n_int line_number);
+
+n_int draw_error(n_constant_string error_text, n_constant_string location, n_int line_number)
+{
+    printf("ERROR: %s, %s line: %ld\n", error_text, location, line_number);
+	return -1;
+}
+
+shared_cycle_state shared_cycle(n_uint ticks, n_byte fIdentification, n_int dim_x, n_int dim_y)
+{
+    return SHARED_CYCLE_OK;
+}
+
+n_int shared_init(n_byte view, n_uint random)
+{
+    return 0;
+}
+
+void shared_close(void)
+{
+
+}
+
+n_int shared_menu(n_int menuValue)
+{
+    return 0;
+}
+
+void shared_rotate(n_double num, n_byte wwind)
+{
+    
+}
+
+void shared_keyReceived(n_byte2 value, n_byte fIdentification)
+{
+    
+}
+
+void shared_keyUp(void)
+{
+    
+}
+
+void shared_mouseOption(n_byte option)
+{
+    
+}
+
+void shared_mouseReceived(n_int valX, n_int valY, n_byte fIdentification)
+{
+
+}
+
+void shared_mouseUp(void)
+{
+    
+}
+
+void shared_about(n_constant_string value)
+{
+    
+}
+
+n_byte * shared_draw(n_byte fIdentification)
+{
+    n_int loop = 0;
+    
+    while (loop < (1024*768))
+    {
+        n_byte value = 0;
+        n_int  y = loop >> 10;
+        if ((loop + y) & 1)
+        {
+            value = 255;
+        }
+        
+        screen[loop++] = value;
+    }
+    
+    return screen;
+}
+
+void shared_color(n_byte2 * fit, n_int fIdentification)
+{
+    n_int loop = 0;
+    n_int fitloop = 0;
+    while (loop < 256)
+    {
+        fit[fitloop++] = loop << 8;
+        fit[fitloop++] = loop << 8;
+        fit[fitloop++] = loop << 8;
+        
+        loop++;
+    }
+
+}
+
+n_int shared_new(n_uint seed)
+{    
+    return 0;
+}
+
+n_byte shared_openFileName(n_string cStringFileName, n_byte isScript)
+{
+    return 0;
+}
+
+void shared_saveFileName(n_string cStringFileName)
+{
+    
+}
+
+void shared_script_debug_handle(n_string cStringFileName)
+{
+    
+}
+
+#ifndef	_WIN32
+
+n_int sim_thread_console_quit(void)
+{
+    return 0;
+}
+
+#endif
+
