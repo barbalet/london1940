9add3907daa9e6fe6785e1d50b3dd862c6d06bd8
Tom Barbalet <tom@nobleape.com>
2014-06-25 00:02:05 -0700

    Simplifying and elongating the house creation
    See title

--- gui/shared.c
+++ gui/shared.c
@@ -160,7 +160,7 @@ n_byte * shared_draw(n_byte fIdentification)
 {
     time_cycle ++;
     
-    if (time_cycle > 12)
+    if (time_cycle > 120)
     {
         time_cycle = 0;
         io_erase(screen, (1024*768));
