eb59c1d7c18216e2bd81335efaa4362c9304d174
Tom Barbalet <tbarbalet@netflix.com>
2018-03-16 11:55:51 +1300

    Add shared_new_agents

--- gui/shared.c
+++ gui/shared.c
@@ -211,6 +211,11 @@ n_int shared_new(n_uint seed)
     return 0;
 }
 
+n_int shared_new_agents(n_uint seed)
+{
+    return 0;
+}
+
 n_byte shared_openFileName(n_string cStringFileName, n_byte isScript)
 {
     return 0;
